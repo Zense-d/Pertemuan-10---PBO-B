2bc7ac5f78d04851d98b466fb6357849f3176a5b
Zense-d <fauzanamandani@gmail.com>
2025-11-03 22:48:19 +0700

    OnlineShop

---
+++ SalesItem.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import javafx.scene.control.TextInputControl;
+
+public class SalesItem
+{
+    private String name;
+    private int price; // in cents
+    private ArrayList<Comment> comments;   
+
+    public SalesItem(String name, int price){
+        this.name = name;
+        this.price = price;
+        comments = new ArrayList<Comment>();
+    }
+        
+    public String getName(){
+        return name;
+    }
+
+    public int getPrice(){
+    return price;
+    }
+
+    public int getNumberOfComments(){
+    return comments.size();
+    }        
+
+    public boolean addComment(String author, String text, int rating){
+        if(rating < 1 || rating > 5) {
+        return false;
+        }
+        
+        for(Comment comment : comments) {
+            if(comment.getAuthor().equals(author)) {
+            return false;
+            }
+        }
+        
+        Comment newComment = new Comment(author, text, rating);
+        comments.add(newComment);
+        return true;
+        }
+
+    public boolean removeComment(Comment comment){
+    return comments.remove(comment);
+    }
+
+    public void showInfo(){
+    System.out.println("=================================");
+    System.out.println("Item: " + name);
+    System.out.println("Price: " + price);
+    System.out.println("Number of comments: " + getNumberOfComments());
+        for(Comment comment : comments) {
+            comment.print();
+            System.out.println();
+        }
+    }
+    
+    public Comment findMostHelpfulComment(){
+        if(comments.isEmpty()) {
+            return null;
+        }
+    Comment best = comments.get(0);
+        for(Comment current : comments) {
+            if(current.getVoteBalance() > best.getVoteBalance()) {
+                best = current;
+            }
+        }
+    return best;
+    }
+
+}    
+
